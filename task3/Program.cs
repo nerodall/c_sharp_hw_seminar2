549d1dea7f517bddf2ca06dc74061b2581215166
Алексей Нерода <neroda.all@mail.ru>
2023-02-15 22:57:24 +0300

    task3 done

---
+++ task3/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число дня недели от 1 до 7");
+int value = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (value > 5 && value < 8)
+{
+    System.Console.WriteLine("этот день недели выходной");
+}
+else
+{
+
+    System.Console.WriteLine("этот день недели не является выходным");
+}
