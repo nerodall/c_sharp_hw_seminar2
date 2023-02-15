75f6fe573b99880ef17ab4a12128b3f0a07cdaa3
Алексей Нерода <neroda.all@mail.ru>
2023-02-15 22:43:42 +0300

    task2 done

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите 3х значное число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+if ( value > 99)
+{
+ value = value % 10;
+ System.Console.WriteLine($"{value} третья цифра введенного числа");
+}
+else
+{
+    System.Console.WriteLine($"{value} не является 3х значным числом");
+}
