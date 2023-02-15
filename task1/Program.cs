7b29fd2fd5323a51037113b5fccfefcc8f666b57
Алексей Нерода <neroda.all@mail.ru>
2023-02-15 22:36:58 +0300

    task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.WriteLine("Введите 3х значное число");
+int value = Convert.ToInt32(Console.ReadLine());
+
+value = value / 10 % 10 ;
+
+System.Console.WriteLine($"{value} это вторая цифра введенного числа");
